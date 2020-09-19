73116ddbbaf7696cc063c263cf78f23f32ef4c0a
Monika <mm@froso.de>
2020-09-19 10:34:17 +0200

    controller

---
+++ FractionCalcOOP/Controller/Controller.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace FractionCalcOOP
+{
+    public class Controller
+    {
+        public Controller()
+        {
+
+        #region atributes
+
+        #endregion
+
+        #region properties
+
+        #endregion
+
+        #region constructors
+
+        #endregion
+
+        #region methods
+
+        #endregion
+    }
+}
+}
